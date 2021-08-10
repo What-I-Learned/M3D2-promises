fab81aaa6c93912ea0e575887f213f86b9d76d1d
js25s <petrauskas.justas@gmail.com>
2021-08-10 15:42:30 +0200

    changing

--- script.js
+++ script.js
@@ -3,6 +3,61 @@ const currentTime = document.querySelector(".curent-time");
 const totalTime = document.querySelector(".total-time");
 const playBtn = document.querySelector(".btn-footer-play > i");
 
+function getArtist() {
+  fetch("https://deezerdevs-deezer.p.rapidapi.com/artist/13", {
+    method: "GET",
+    headers: {
+      "x-rapidapi-key": "9835f40629mshe9f0744f8b07fe0p13e171jsn785e2f64657a",
+      "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+    },
+  })
+    .then((response) => response.json())
+    .then((artistData) => {
+      displayArtist(artistData);
+      console.log(artistData);
+      getTopTracks(artistData.tracklist);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+  console.log("artist loading");
+}
+
+function getTopTracks(url) {
+  fetch(url, {
+    method: "GET",
+    headers: {
+      "x-rapidapi-key": "9835f40629mshe9f0744f8b07fe0p13e171jsn785e2f64657a",
+      "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+    },
+  })
+    .then((response) => response.json())
+    .then((topTracksData) => {
+      console.log(topTracksData);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+
+function displayArtist(artist) {
+  let artistPage = document.querySelector(".artist-card-body");
+  let artistImage = document.querySelector(".artist-coverImg");
+
+  artistImage.innerHTML += `<img src=${artist.picture_xl} class="card-img" alt="..." />`;
+
+  artistPage.innerHTML += `
+  <span
+  ><i class="fas fa-certificate" style="font-size: 14px"></i>
+  <p>Verified Artist</p></span
+>
+<h1 class="album-card-title">${artist.name}</h1>
+<p class="album-card-text m-0">
+  <span>${artist.nb_fan}</span> montly listeners
+</p>
+          `;
+}
+
 playBtn.addEventListener("click", function () {
   if (playBtn.className === "fas fa-play-circle") {
     playBtn.classList.remove("fa-play-circle");
@@ -35,4 +90,5 @@ window.onload = function (event) {
   playbackTime.style.backgroundSize = `0% 100%`;
   playbackTime.setAttribute("value", "0");
   console.log("page is fully loaded", event.curr);
+  getArtist();
 };
